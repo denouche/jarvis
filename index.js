64f375973517a0bec80137f80ecaab657d1e58ee
antoine.leveugle <antoine.leveugle@gmail.com>
2017-01-02 17:09:28 +0100

    feat: add hello plugin, update dependencies

--- index.js
+++ index.js
@@ -6,7 +6,8 @@ const VirtualAssistant = require('virtual-assistant').VirtualAssistant;
 
 let featureList = [
         require('virtual-assistant-plugin-tictactoe'),
-        require('virtual-assistant-plugin-regexp-challenge')
+        require('virtual-assistant-plugin-regexp-challenge'),
+        require('virtual-assistant-plugin-hello-world')
     ],
     options = {
         slack: { token: process.env.SLACK_TOKEN }
