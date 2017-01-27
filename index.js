9ec553d10e882537301aeace17dccb88026caf57
antoine.leveugle <antoine.leveugle@gmail.com>
2017-01-27 21:37:32 +0100

    feat: add sed challenge

--- index.js
+++ index.js
@@ -7,12 +7,13 @@ const VirtualAssistant = require('virtual-assistant').VirtualAssistant;
 let featureList = [
         require('virtual-assistant-plugin-tictactoe'),
         require('virtual-assistant-plugin-regexp-challenge'),
+        require('virtual-assistant-plugin-sed-challenge'),
         require('virtual-assistant-plugin-hello-world')
     ],
     options = {
         slack: {
-        	token: process.env.SLACK_TOKEN,
-        	administrators: []
+            token: process.env.SLACK_TOKEN,
+            administrators: ['U2Q4ALC6B' /* eli */, 'U0DHA6T5L' /* group */]
         }
     };
 
